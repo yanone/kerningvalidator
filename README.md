1941693a15a05144ac0b26b471e722ba3dc33a32
Yanone <post@yanone.de>
2022-10-25 16:59:26 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# kerningvalidator
+# Kerning Validator
+
+Compares kerning defined in `.glyphs` source with kerning defined in binary font.
+This is currently limited to encoded glyphs only.
+
+It reads all kerning pairs of all masters of a Variable Font from the `.glyphs` source
+and shapes the pairs using the binary font with `vharfbuzz`, once with and once without 
+kerning enabled, then compares the two values.
+
+To catch absolutely all kerning, it gathers all unicodes associated with a kerning class
+and cross-checks all possible combinations. This is necessary because members of a kerning class
+may be part of different writing scripts.
+
+## Invocation
+
+From the command line:
+`kerningvalidator -s font.glyphs -b font.ttf`
+
+From within Python:
+```python
+import kerningvalidator
+
+missing_kerning = list(kerningvalidator.missing_kerning(source_path, binary_path))
+```
+
+## Limitations
+
+This tool was written with Variable Fonts in mind that contain both `LTR` and `RTL` kerning pairs.
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import platform
+
+WIN = platform.system() == "Windows"
+MAC = platform.system() == "Darwin"
+LINUX = platform.system() == "Linux"
+
+install_requires = [
+    line for line in open("requirements.txt").readlines() if bool(line.strip()) and not line.strip().startswith("#")
+]
+
+setup(
+    name="kerningvalidator",  # How you named your package folder (MyLib)
+    version="0.1.0",  # .post2
+    license="apache-2.0",
+    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    author="Yanone",  # Type in your name
+    author_email="post@yanone.de",  # Type in your E-Mail
+    url="https://github.com/yanone/kerningvalidator",
+    # Provide either the link to your github or to your website
+    keywords=["fonts"],  # Keywords that define your package best
+    install_requires=install_requires,
+    entry_points={
+        "console_scripts": [
+            "kerningvalidator = kerningvalidator.cli:main",
+        ]
+    },
+    package_dir={"": "Lib"},
+    packages=find_packages("Lib"),
+    include_package_data=True,
+)
