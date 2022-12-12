763cf86ab92e040b76efcd94042c28a006efc8e6
Yanone <post@yanone.de>
2022-12-12 08:46:02 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install_requires = [
 
 setup(
     name="kerningvalidator",  # How you named your package folder (MyLib)
-    version="0.1.0",  # .post2
+    version="0.1.1",  # .post2
     license="apache-2.0",
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     author="Yanone",  # Type in your name
